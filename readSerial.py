73acc4b8f22a452f3e9c1b98f388ba8c5cef27af
nodice73 <adam.james.waite@gmail.com>
2016-08-13 18:52:51 -0400

    updated for python 3 and simplified.

--- readSerial.py
+++ readSerial.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env/ python
 
-from serial import *
+from serial import Serial
 import sys
 
 serial_port = "/dev/ttyUSB0"
@@ -9,7 +9,4 @@ ser = Serial(serial_port, baud_rate, timeout=0, writeTimeout=0)
 
 while True:
     c = ser.readline()
-    if c == '\r' or c == 0:
-        c = ''
-    sys.stdout.write(c)
-    sys.stdout.flush()
+    sys.stdout.write(str(c, 'utf-8'))
